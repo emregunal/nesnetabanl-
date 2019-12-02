4e2c81a8fc739ca172f2d4be044594d356f955cb
Emre Günal <emregunnal@gmail.com>
2019-12-02 15:21:39 +0300

    a1

--- 021219ders1/src/evcilHayvan.java
+++ 021219ders1/src/evcilHayvan.java
@@ -3,7 +3,7 @@ public class evcilHayvan{
 	private String  isim;
 	public evcilHayvan(String isim){
 		this.isim=isim;
-		System.out.println(this.isim+" oynamak istiyor.");
+		System.out.println(this.isim+" .oynamak istiyor.");
 	}
     public String oyna()
     {
